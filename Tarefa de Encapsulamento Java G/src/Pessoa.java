241fe45a0b7a3aa91f4678a10a8bb827057d017e
Greicy Matias <111903522+Greicy28@users.noreply.github.com>
2023-07-15 17:44:17 -0300

    Add files via upload

---
+++ Tarefa de Encapsulamento Java G/src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa{
+private String nome="Greicy";
+private int idade=26;
+private int numtelefone=987935678;
+
+public void informacoesPessoa(){
+System.out.println("O nome da pessoa é: "+nome);
+System.out.println("A sua idade é: "+idade+" anos");
+System.out.print("Seu número de telefone é: "+numtelefone);
+}
+}
